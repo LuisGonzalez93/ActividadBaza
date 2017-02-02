806f8cc070b4ef1bdb64509d65c833407c912e8f
LuisGonzalez93 <lg02393@gmail.com>
2017-02-02 13:12:05 +0100

    Añadidos los metodos getPaloPrimeraCartaDeLaBaza y cartaQueVaGanandoLaBaza

--- Baza.java
+++ Baza.java
@@ -7,11 +7,14 @@
  */
 public class Baza
 {
-    
+
     private int numeroJugadores;
-    private int palo;
+
     private Carta[] baza;
-    
+
+    private Carta cartaganadora;
+
+    private int palo;
 
     /**
      * Palo que pinta: 0 es oros, 1 es copas, 2 es espadas y 3 es bastos
@@ -25,9 +28,29 @@ public class Baza
 
     public void addCarta (Carta cartaTirada, String nombreJugador){
         int i = 0;
+        nombreJugador = nombreJugador;
         baza[i] = cartaTirada;
+
         i++;
     }
-    
-   
+
+    public int getPaloPrimeraCartaDeLaBaza(){
+        int paloQuePinta = -1;
+        paloQuePinta = baza[0].getPalo();
+
+        return paloQuePinta;
+
+    }
+
+    public Carta cartaQueVaGanandoLaBaza(){
+        for(int i = 0; i < numeroJugadores; i++){
+            if(baza[i] == null){
+                cartaganadora = null;
+
+            }
+            
+        }
+        return cartaganadora;
+
+    }
 }
