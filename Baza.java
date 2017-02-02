577c345dfa5516c1cf84d48b81f64d3e3bc476c7
LuisGonzalez93 <lg02393@gmail.com>
2017-02-02 10:50:27 +0100

    Añadido el constructor

---
+++ Baza.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author (Luis Gonzalez) 
+ * @version (a version number or a date)
+ */
+public class Baza
+{
+    
+    private int numeroJugadores;
+    private int palo;
+    
+
+    /**
+     * Palo que pinta: 0 es oros, 1 es copas, 2 es espadas y 3 es bastos
+     */
+    public Baza(int numeroJugadores , int paloQuePinta)
+    {
+        this.numeroJugadores = numeroJugadores;
+        this.palo = paloQuePinta;
+        
+    }
+
+   
+    
+   
+}
